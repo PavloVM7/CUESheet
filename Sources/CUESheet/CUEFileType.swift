a97cc45b10d33566584ab33c98f2d7ba8bbeb504
Pavlo Moisieienko <pavel.moyseyenko@yahoo.com>
2023-08-11 21:06:47 +0300

    new: cue file types enumeration

---
+++ Sources/CUESheet/CUEFileType.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pavlo Moisieienko on 11.08.2023.
+//
+
+import Foundation
+
+public enum CUEFileType {
+    case UNKNOWN, WAVE, MP3
+    static func from(_ name: String) -> CUEFileType {
+        switch name.uppercased() {
+        case "WAVE":
+            return .WAVE
+        case "MP3":
+            return .MP3
+        default:
+            return .UNKNOWN
+        }
+    }
+}
---
+++ Tests/CUESheetTests/CUEFileType_Test.swift
@@ -0,0 +1,26 @@
+//
+//  CUEFileType_Test.swift
+//  
+//
+//  Created by Pavlo Moisieienko on 11.08.2023.
+//
+
+import XCTest
+@testable import CUESheet
+final class CUEFileType_Test: XCTestCase {
+
+    func test_UNKNOWN() throws {
+        let names = ["wave", "WAVE", "Wave",
+                     "MP3", "mp3", "Mp3",
+                     "unknown", "any", "Whatever"]
+        let expected = [CUEFileType.WAVE, CUEFileType.WAVE, CUEFileType.WAVE,
+                        CUEFileType.MP3, CUEFileType.MP3, CUEFileType.MP3,
+                        CUEFileType.UNKNOWN, CUEFileType.UNKNOWN, CUEFileType.UNKNOWN]
+        for i in 0..<names.count {
+            let name = names[i]
+            let expected = expected[i]
+            let actual = CUEFileType.from(name)
+            XCTAssertEqual(actual, expected, "expected: \(expected), actual: \(actual)")
+        }
+    }
+}
